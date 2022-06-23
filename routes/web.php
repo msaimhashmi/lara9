142829ab890b57ec2dbcde73c1e3c32cf9d5a8a1
msaimhashmi <msaimhashmi99@gmail.com>
2022-06-23 07:34:15 -0700

    two new helper functions

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// 	TWO NEW HELPER FUNCTIONS (Mentioned in routes and also committed seperately)
+
 Route::get('/', function () {
+
+	// Before laravel 9
+	// return Str::of('Hello World')->upper();
+	// return Str::slug('Hello World');
+	
+	// In laravel 9 (NEW HELPER FUNCTION - str to operate on string in a more object oriented or even a pure way)
+	// return str('Hello World')->upper();
+	// return str('Hello World')->slug();
+
     return view('welcome');
+})->name('home');
+
+Route::get('/endpoint', function(){
+
+	// Before laravel 9
+	// return redirect()->route('home');
+
+	// In laravel 9 (NEW HELPER FUNCTION)
+	return to_route('home');
+
 });
+
+
 
 
 // Before laravel 9
@@ -24,7 +48,7 @@ Route::get('/', function () {
 // Route::get('/posts/{post}', [PostsController::class, 'show']);
 // Route::post('/posts', [PostsController::class, 'store']);
 
-// After laravel 9 (CONTROLLER ROUTE GROUP)
+// In laravel 9 (CONTROLLER ROUTE GROUP)
 Route::controller(PostsController::class)->group(function(){
 	Route::get('/posts', 'index');
 	Route::get('/posts/{post}', 'show');
